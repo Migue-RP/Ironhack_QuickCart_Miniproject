9ba78c8f729ee25abfac1b64c26fe0fd71132f92
Miguel <miguerp89@hotmail.com>
2026-05-18 08:51:36 +0200

    Delete Section D - ETL_ELT + Airflow + dbt/dbt_models directory
    new update was uploaded

--- Section D - ETL_ELT + Airflow + dbt/dbt_models/staging/stg_products.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    product_id,
-    TRIM(name) AS product_name,
-    UPPER(TRIM(category)) AS category,
-    CAST(price_usd AS NUMERIC(10,2)) AS price_usd,
-    CAST(cost_usd AS NUMERIC(10,2)) AS cost_usd
-FROM {{ source('quickcart_raw', 'products') }}
